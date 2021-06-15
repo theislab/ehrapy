8fa735abbc6cb09b9769075bf1dd3f6d1351476d
zethson <lukas.heumos@posteo.net>
2021-06-15 14:08:27 +0200

    add version check on import
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- ehrapy/__init__.py
+++ ehrapy/__init__.py
@@ -3,3 +3,8 @@
 __author__ = "Lukas Heumos"
 __email__ = "lukas.heumos@posteo.net"
 __version__ = "0.1.0"
+
+from ehrapy.api import preprocessing, plot, tools
+from ehrapy.cli.upgrade import UpgradeCommand
+
+UpgradeCommand.check_ehrapy_latest()
