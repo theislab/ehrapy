f5c7775093fb54b69ca407ca09f26384bb9f8e3e
zethson <lukas.heumos@posteo.net>
2021-11-10 17:42:15 +0100

    nox
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- ehrapy/api/plot/__init__.py
+++ ehrapy/api/plot/__init__.py
@@ -1,2 +1,2 @@
-from ehrapy.api.plot._scanpy_pl_api import *  # noqa: E402,F403
 from ehrapy.api.plot._plot_util import *  # noqa: E402,F403
+from ehrapy.api.plot._scanpy_pl_api import *  # noqa: E402,F403
--- ehrapy/api/plot/_plot_util.py
+++ ehrapy/api/plot/_plot_util.py
@@ -16,7 +16,5 @@ class Colormaps(Enum):
     grey_red = LinearSegmentedColormap.from_list("grouping", ["lightgray", "red", "darkred"], N=128)
     grey_green = LinearSegmentedColormap.from_list("grouping", ["lightgray", "limegreen", "forestgreen"], N=128)
     grey_yellow = LinearSegmentedColormap.from_list("grouping", ["lightgray", "yellow", "gold"], N=128)
-    grey_violet = LinearSegmentedColormap.from_list(
-        "grouping", ["lightgray", "mediumvioletred", "indigo"], N=128
-    )
+    grey_violet = LinearSegmentedColormap.from_list("grouping", ["lightgray", "mediumvioletred", "indigo"], N=128)
     grey_blue = LinearSegmentedColormap.from_list("grouping", ["lightgray", "cornflowerblue", "darkblue"], N=128)
