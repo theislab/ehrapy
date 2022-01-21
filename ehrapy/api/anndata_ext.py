[Fix] #262: Fix get numerical vars for normalization
- numerical vars are now stored in uns when reading

- updated caching to keep those numerical values in uns

- updated tests to reflect new changes

ehrapy/api/anndata_ext.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections import OrderedDict
 from typing import NamedTuple
 
 import numpy as np
@@ -32,13 +33,18 @@ def df_to_anndata(
     # if data is numerical only, short-circuit AnnData creation to have float dtype instead of object
     all_num = all(np.issubdtype(column_dtype, np.number) for column_dtype in dataframes.df.dtypes)
     X = dataframes.df.to_numpy(copy=True)
-
+    # initializing an OrderedDict with a non-empty dict might not be intended,
+    # see: https://stackoverflow.com/questions/25480089/right-way-to-initialize-an-ordereddict-using-its-constructor-such-that-it-retain/25480206
+    uns = OrderedDict()
+    # store all numerical columns (used for normalization)
+    uns["numerical_columns"] = list(df.select_dtypes("number").columns)
     return AnnData(
         X=X,
         obs=dataframes.obs,
         var=pd.DataFrame(index=list(dataframes.df.columns)),
         dtype="float32" if all_num else "object",
         layers={"original": X.copy()},
+        uns=uns,
     )
 
 
@@ -131,7 +137,7 @@ def get_column_values(adata: AnnData, indices: int | list[int]) -> np.ndarray:
 
 def assert_encoded(adata: AnnData):
     try:
-        assert "categoricals" in adata.uns_keys()
+        assert any(enc_flag in adata.uns_keys() for enc_flag in ["categoricals", "encoding_to_var"])
     except AssertionError:
         raise NotEncodedError("The AnnData object has not yet been encoded.") from AssertionError
 
@@ -143,11 +149,11 @@ def get_numeric_vars(adata: AnnData) -> list[str]:
         adata: :class:`~anndata.AnnData` object
 
     Returns:
-        Set of column numeric column names
+        List of column numeric column names
     """
     assert_encoded(adata)
 
-    return adata.uns["categoricals"]["not_categorical"]
+    return adata.uns["numerical_columns"]
 
 
 def assert_numeric_vars(adata: AnnData, vars: list[str]):

tests/api/preprocessing/test_normalization.py
@@ -1,4 +1,5 @@
 import warnings
+from collections import OrderedDict
 from pathlib import Path
 
 import numpy as np
@@ -28,14 +29,14 @@ class TestNormalization:
             "Feature": ["Integer1", "Numeric1", "Numeric2", "Numeric3", "String1", "String2"],
             "Type": ["Integer", "Numeric", "Numeric", "Numeric", "String", "String"],
         }
-
         self.adata = AnnData(
             X=X_data,
             obs=pd.DataFrame(data=obs_data),
             var=pd.DataFrame(data=var_data, index=var_data["Feature"]),
             dtype=np.dtype(object),
+            uns=OrderedDict(),
         )
-
+        self.adata.uns["numerical_columns"] = ["Numeric1", "Numeric2"]
         self.adata = ep.pp.encode(self.adata, autodetect=True, encodings={})
 
     def test_vars_checks(self):

tests/api/test_anndata_ext.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from typing import Tuple
 
 import numpy as np
@@ -147,15 +148,17 @@ class TestAnnDataUtil:
             obs=pd.DataFrame(data=obs_data),
             var=pd.DataFrame(data=var_numeric, index=var_numeric["Feature"]),
             dtype=np.dtype(object),
+            uns=OrderedDict(),
         )
 
+        self.adata_numeric.uns["numerical_columns"] = ["Numeric1", "Numeric2"]
         self.adata_strings = AnnData(
             X=X_strings,
             obs=pd.DataFrame(data=obs_data),
             var=pd.DataFrame(data=var_strings, index=var_strings["Feature"]),
             dtype=np.dtype(object),
         )
-
+        self.adata_strings.uns["numerical_columns"] = ["Numeric1", "Numeric2"]
         self.adata_encoded = ep.pp.encode(self.adata_strings.copy(), autodetect=True, encodings={})
 
     def test_assert_encoded(self):
@@ -171,7 +174,7 @@ class TestAnnDataUtil:
     def test_get_numeric_vars(self):
         """Test for the numeric vars getter."""
         vars = get_numeric_vars(self.adata_encoded)
-        assert vars == ["Numeric2"]
+        assert vars == ["Numeric1", "Numeric2"]
 
         with pytest.raises(NotEncodedError, match=r"not yet been encoded"):
             get_numeric_vars(self.adata_numeric)
@@ -181,7 +184,7 @@ class TestAnnDataUtil:
 
     def test_assert_numeric_vars(self):
         "Test for the numeric vars assertion."
-        assert_numeric_vars(self.adata_encoded, ["Numeric2"])
+        assert_numeric_vars(self.adata_encoded, ["Numeric1", "Numeric2"])
 
         with pytest.raises(ValueError, match=r"Some selected vars are not numeric"):
             assert_numeric_vars(self.adata_encoded, ["Numeric2", "String1"])
@@ -189,15 +192,13 @@ class TestAnnDataUtil:
     def test_set_numeric_vars(self):
         """Test for the numeric vars setter."""
         values = np.array(
-            [
-                [1.2],
-                [2.2],
-                [2.2],
-            ],
+            [[1.2, 2.2], [3.2, 4.2], [5.2, 6.2]],
             dtype=np.dtype(np.float32),
         )
         adata_set = set_numeric_vars(self.adata_encoded, values, copy=True)
-        assert (adata_set.X[:, 3] == values[:, 0]).all()
+        np.testing.assert_array_equal(adata_set.X[:, 2], values[:, 0]) and np.testing.assert_array_equal(
+            adata_set.X[:, 3], values[:, 1]
+        )
 
         with pytest.raises(ValueError, match=r"Some selected vars are not numeric"):
             set_numeric_vars(self.adata_encoded, values, vars=["ehrapycat_String1"])
@@ -215,9 +216,9 @@ class TestAnnDataUtil:
 
         extra_values = np.array(
             [
-                [1.2, 1.3],
-                [2.2, 2.3],
-                [2.2, 2.3],
+                [1.2, 1.3, 1.4],
+                [2.2, 2.3, 2.4],
+                [2.2, 2.3, 2.4],
             ],
             dtype=np.dtype(np.float32),
         )