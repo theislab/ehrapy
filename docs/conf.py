02b78012b04a1e33bf7f921906e31f10ff21e5d0
zethson <lukas.heumos@posteo.net>
2022-02-01 09:42:26 +0100

    Bump version from 0.1.0 to 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ copyright = "2021, Lukas Heumos, Theislab"
 author = "Lukas Heumos"
 github_repo = "ehrapy"
 
-version = "0.1.0"
-release = "0.1.0"
+version = "0.2.0"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------
 extensions = [
