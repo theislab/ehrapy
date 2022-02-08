a639ab9c3f1f74dd3379e5f9ee8a70532ccaf966
zethson <lukas.heumos@posteo.net>
2022-02-08 13:19:01 +0100

    fix nox & patient id
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- ehrapy/data/__init__.py
+++ ehrapy/data/__init__.py
@@ -1,1 +1,1 @@
-from ehrapy.data._datasets import diabetes_130, heart_failure, mimic_2, mimic_3_demo, chronic_kidney_disease
+from ehrapy.data._datasets import chronic_kidney_disease, diabetes_130, heart_failure, mimic_2, mimic_3_demo
--- ehrapy/data/_datasets.py
+++ ehrapy/data/_datasets.py
@@ -151,16 +151,15 @@ def diabetes_130(
 
 
 def chronic_kidney_disease(
-    encoded: bool = False,
     columns_obs_only: dict[str, list[str]] | list[str] | None = None,
 ) -> AnnData:  # pragma: no cover
     """Loads the Chronic Kidney Disease dataset
 
     More details: https://archive.ics.uci.edu/ml/datasets/Chronic_Kidney_Disease
     Preprocessing: https://github.com/theislab/ehrapy-datasets/tree/main/chronic_kidney_disease/chronic_kidney_disease.ipynb
-    
+
     Args:
-        encoded: Whether to return an already encoded object
+        columns_obs_only: Columns to include in obs only and not X.
 
     Returns:
         :class:`~anndata.AnnData` object of the Chronic Kidney Disease dataset
@@ -168,37 +167,17 @@ def chronic_kidney_disease(
     Example:
         .. code-block:: python
 
-            import ehrapy.api as ep
+            import ehrapy as ep
 
-            adata = ep.data.chronic_kidney_disease(encoded=True)
+            adata = ep.data.chronic_kidney_disease()
     """
     adata = read(
         dataset_path=f"{ehrapy_settings.datasetdir}/chronic_kidney_disease_precessed.csv",
         download_dataset_name="chronic_kidney_disease.csv",
-        backup_url="",
+        backup_url="https://figshare.com/ndownloader/files/33989261",
         columns_obs_only=columns_obs_only,
+        extension="csv",
+        index_column="Patient_id",
     )
-    if encoded:
-        return encode(
-            adata,
-            encodings={
-                "one_hot_encoding": [
-                    "Specific Gravity",
-                    "Albumin",
-                    "Sugar",
-                    "Red Blood Cells",
-                    "Pus Cell",
-                    "Pus Cell clumps",
-                    "Bacteria",
-                    "Hypertension",
-                    "Diabetes Mellitus",
-                    "Coronary Artery Disease",
-                    "Appetite",
-                    "Pedal Edema",
-                    "Anemia",
-                    "Class",
-                ]
-            },
-        )
 
     return adata
