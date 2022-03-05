23b85a7a4256b5637108b00ac815f27818221c74
xinyuejohn <xinyue.zhang@tum.de>
2022-03-05 23:38:17 +0100

    Add encoded parameter to all new datasets amd fix import

--- ehrapy/data/__init__.py
+++ ehrapy/data/__init__.py
@@ -1,1 +1,14 @@
-from ehrapy.data._datasets import chronic_kidney_disease, diabetes_130, heart_failure, mimic_2, mimic_3_demo
+from ehrapy.data._datasets import (
+    breast_tissue,
+    cervical_cancer_risk_factors,
+    chronic_kidney_disease,
+    dermatology,
+    diabetes_130,
+    echocardiogram,
+    heart_failure,
+    hepatitis,
+    mimic_2,
+    mimic_3_demo,
+    statlog_heart,
+    thyroid,
+)
