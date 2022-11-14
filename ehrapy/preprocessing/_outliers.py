2dadabea513aa2165342639662ea9a53c0407a77
Altana Namsaraeva <namsaraeva@cip.ifi.lmu.de>
2022-11-14 15:51:54 +0100

    Added a skeleton for filter_quantiles(), edited the docs

--- ehrapy/preprocessing/_outliers.py
+++ ehrapy/preprocessing/_outliers.py
@@ -4,7 +4,11 @@ from anndata import AnnData
 
 
 def winsorize(
-    adata: AnnData, vars: list[str], obs_cols: list[str], limits: list[float], copy: bool = False, **kwargs
+    adata: AnnData,
+    vars: str | list[str],
+    obs_cols: str | list[str],
+    limits: list[float],
+    copy: bool = False, **kwargs
 ) -> AnnData:
     """Returns a Winsorized version of the input array.
 
@@ -13,6 +17,7 @@ def winsorize(
     Args:
         adata: AnnData object to winsorize
         vars: The features to winsorize
+        obs_cols: Columns in obs with features to winsorize
         limits: Tuple of the percentages to cut on each side of the array as floats between 0. and 1.
         copy: Whether to return a copy or not
         **kwargs: Keywords arguments get passed to scipy.stats.mstats.winsorize
@@ -20,17 +25,107 @@ def winsorize(
     Returns:
         Winsorized AnnData object if copy is True
     """
+    if not all(elem in adata.var_names.values for elem in vars):
+        raise ValueError(
+            f"Columns `{[col for col in vars if col not in adata.var_names.values]}` are not in var_names."
+        )
+
+    if not all(elem in adata.obs.columns.values for elem in obs_cols):
+        raise ValueError(
+            f"Columns `{[col for col in obs_cols if col not in adata.obs.columns.values]}` are not in obs."
+        )
+
+    if isinstance(vars, str):  # pragma: no cover
+        vars = [vars]
+
+    if isinstance(obs_cols, str):  # pragma: no cover
+        obs_cols = [obs_cols]
+
     if copy:  # pragma: no cover
         adata = adata.copy()
 
     if copy:
         return adata
 
+    pass
+
 
 def clip_quantile(
     adata: AnnData,
     vars: list[str],
     obs_cols: list[str],
-):
+    limits: list[float],
+    copy: bool = False
+) -> AnnData | None:
+    """Clips (limits) features
+
+    Args:
+        adata: The AnnData object
+        vars: Columns in var with features to clip
+        obs_cols: Columns in obs with features to clip
+        limits: Interval, values outside of which are clipped to the interval edges
+        copy: Whether to return a copy of AnnData or not
+
+    Returns:
+        A copy of original AnnData object with clipped features.
+    """
     # https://numpy.org/doc/stable/reference/generated/numpy.clip.html
+    if not all(elem in adata.var_names.values for elem in vars):
+        raise ValueError(
+            f"Columns `{[col for col in vars if col not in adata.var_names.values]}` are not in var_names."
+        )
+
+    if not all(elem in adata.obs.columns.values for elem in obs_cols):
+        raise ValueError(
+            f"Columns `{[col for col in obs_cols if col not in adata.obs.columns.values]}` are not in obs."
+        )
+
+    if isinstance(vars, str):  # pragma: no cover
+        vars = [vars]
+
+    if isinstance(obs_cols, str):  # pragma: no cover
+        obs_cols = [obs_cols]
+
+    if copy:  # pragma: no cover
+        adata = adata.copy()
+
+    if copy:
+        return adata
+
+    pass
+
+
+def filter_quantiles(
+    adata: AnnData,
+    vars: str | list[str],
+    quantile_top: int | None,
+    quantile_bottom: int | None,
+    copy: bool = False
+) -> AnnData | None:
+    """Filter numeric features by top/bottom quantiles
+
+    Args:
+        adata: The AnnData object
+        vars: Columns with features to filter
+        quantile_top: Remove the top % largest values.
+        quantile_bottom: Remove the bottom % lowest values.
+        copy: Whether to return a copy of AnnData or not
+
+    Returns:
+        A copy of original AnnData object with filtered features.
+    """
+    if not all(elem in adata.var_names.values for elem in vars):
+        raise ValueError(
+            f"Columns `{[col for col in vars if col not in adata.var_names.values]}` are not in var_names."
+        )
+
+    if isinstance(vars, str):  # pragma: no cover
+        vars = [vars]
+
+    if copy:  # pragma: no cover
+        adata = adata.copy()
+
+    if copy:
+        return adata
+
     pass
