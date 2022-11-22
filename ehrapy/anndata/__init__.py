e1515b0f14b1a24fa230ae83ba1066cea46117f4
Altana Namsaraeva <namsaraeva@cip.ifi.lmu.de>
2022-11-22 13:54:35 +0100

    precommit fix

--- ehrapy/anndata/__init__.py
+++ ehrapy/anndata/__init__.py
@@ -1,9 +1,9 @@
 from ehrapy.anndata.anndata_ext import (
     anndata_to_df,
+    delete_from_obs,
     df_to_anndata,
     generate_anndata,
     move_to_obs,
-    delete_from_obs,
     move_to_x,
     type_overview,
 )
