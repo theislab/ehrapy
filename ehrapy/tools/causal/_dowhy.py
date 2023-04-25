6b5175469afc2d507d61854da7e4cb3dd1f18b76
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2023-04-25 05:26:40 +0200

    improved, tests incoming

--- ehrapy/tools/causal/_dowhy.py
+++ ehrapy/tools/causal/_dowhy.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from typing import Tuple, Union, Literal, Dict, List
+from typing import Any, Optional, Tuple, Union, Literal, Dict, List
 
+import io
+from contextlib import redirect_stdout
 import dowhy
 import anndata
 
@@ -27,40 +29,32 @@ def causal_inference(
         "backdoor.panel_regression"
     ] = "backdoor.propensity_score_stratification",
     return_as: Literal["summary", "estimate", "refute_results", "all"] = "summary",
+    # optional kwargs arguments
+    identify_kwargs: Optional[Dict[str, Any]] = None,
+    estimate_kwargs: Optional[Dict[str, Any]] = None,
+    refute_kwargs: Optional[Dict[str, Any]] = None,
 ) -> Tuple[dowhy.CausalEstimate, Dict[str, Union[str, Dict[str, float]]]]:
     """
-    Perform causal inference analysis using the DoWhy library.
+    Performs causal inference on an AnnData object using the specified causal model and returns a tuple containing the causal estimate and the results of any refutation tests.
 
-    Parameters
-    ----------
-    adata : anndata.AnnData
-        An AnnData object containing the data to be analyzed.
-    graph : str
-        A string specifying the causal graph to be used.
-    treatment : str
-        A string specifying the column name of the treatment variable in the data.
-    outcome : str
-        A string specifying the column name of the outcome variable in the data.
-    refute_methods : List[Literal["placebo_treatment_refuter", "random_common_cause", "data_subset_refuter"]], optional
-        A list of strings specifying the methods to be used to refute the causal effect estimate, by default ["placebo_treatment_refuter", "random_common_cause", "data_subset_refuter"].
-    estimation_method : Literal["backdoor.propensity_score_stratification", "backdoor.propensity_score_weighting", "backdoor.linear_regression", "backdoor.propensity_score_matching", "backdoor.frontdoor_propensity_score_matching", "backdoor.instrumental_variable", "backdoor.doubly_robust_weighting", "backdoor.panel_regression"], optional
-        A string specifying the method to be used to estimate the causal effect, by default "backdoor.propensity_score_stratification".
-    return_as : Literal["summary", "estimate", "refute_results", "all"], optional
-        A string specifying the type of result to be returned, by default "summary".
+    Args:
+        adata: An AnnData object containing the input data.
+        graph: A str representing the causal graph to use.
+        treatment: A str representing the treatment variable in the causal graph.
+        outcome: A str representing the outcome variable in the causal graph.
+        refute_methods: An optional List of Literal specifying the methods to use for refutation tests. Defaults to ["placebo_treatment_refuter", "random_common_cause", "data_subset_refuter"].
+        estimation_method: An optional Literal specifying the estimation method to use. Defaults to "backdoor.propensity_score_stratification".
+        return_as: An optional Literal specifying the type of output to return. Defaults to "summary".
+        identify_kwargs: Optional keyword arguments for dowhy.CausalModel.identify_effect().
+        estimate_kwargs: Optional keyword arguments for dowhy.CausalModel.estimate_effect().
+        refute_kwargs: Optional keyword arguments for dowhy.CausalModel.refute_estimate().
 
-    Returns
-    -------
-    Tuple[dowhy.CausalEstimate, Dict[str, Union[str, Dict[str, float]]]] or None
-        Depending on the value of `return_as`, returns a tuple of a `dowhy.CausalEstimate` object and a dictionary of refutation results, or None.
+    Returns:
+        A tuple containing the causal estimate and a dictionary of the results of any refutation tests.
 
-    Raises
-    ------
-    ValueError
-        If an error occurs during the causal inference analysis.
-
-    Notes
-    -----
-    This function uses the DoWhy library to perform causal inference analysis. It first creates a `dowhy.CausalModel` object using the specified data and causal graph, and then identifies and estimates the causal effect of the treatment variable on the outcome variable using the specified method. It then refutes the causal effect estimate using the specified methods, and returns a summary of the results or the full results depending on the value of `return_as`.
+    Raises:
+        TypeError: If adata, graph, treatment, outcome, refute_methods, estimation_method, or return_as is not of the expected type.
+        ValueError: If refute_methods or estimation_method contains an unknown value, or if return_as is an unknown value.
     """
 
     if not isinstance(adata, anndata.AnnData):
@@ -103,20 +97,40 @@ def causal_inference(
     if return_as not in ["summary", "estimate", "refute_results", "all"]:
         raise ValueError(f"Unknown value for return_as {return_as}")
 
+        # Define dictionary containing valid kwargs for each function
+    valid_kwargs = {
+        "identify_effect": ["proceed_when_unidentifiable"],
+        "estimate_effect": ["method_name"],
+        "refute_estimate": ["method_name", "confounders_frontdoor", "confounders_backdoor"],
+    }
+
     # Create causal model
     model = dowhy.CausalModel(data=adata.to_df(), graph=graph, treatment=treatment, outcome=outcome)
 
     # Identify effect
-    identified_estimand = model.identify_effect(proceed_when_unidentifiable=True)
+    identify_kwargs = identify_kwargs or {}
+    assert set(identify_kwargs.keys()).issubset(valid_kwargs["identify_effect"]), "Invalid identify_kwargs"
+    identified_estimand = model.identify_effect(**identify_kwargs)
+
+    # Identify effect
+    # identified_estimand = model.identify_effect(proceed_when_unidentifiable=True)
 
     # Estimate effect
-    estimate = model.estimate_effect(identified_estimand, method_name=estimation_method)
+    estimate_kwargs = estimate_kwargs or {}
+    assert set(estimate_kwargs.keys()).issubset(valid_kwargs["estimate_effect"]), "Invalid estimate_kwargs"
+    estimate = model.estimate_effect(identified_estimand, method_name=estimation_method, **estimate_kwargs)
 
+    # estimate = model.estimate_effect(identified_estimand, method_name=estimation_method)
+    
     # Refute estimate using specified methods
     refute_results = {}
+    output_buffer = io.StringIO()
     for method in refute_methods:
+        refute_kwargs = refute_kwargs or {}
+        assert set(refute_kwargs.keys()).issubset(valid_kwargs["refute_estimate"]), "Invalid refute_kwargs"
         try:
-            refute = model.refute_estimate(identified_estimand, estimate, method_name=method)
+            with redirect_stdout(output_buffer):
+                refute = model.refute_estimate(identified_estimand, estimate, method_name=method, verbose=False, **refute_kwargs)
             test_significance = refute.estimated_effect
 
             refute_results[str(refute.refutation_type)] = {
@@ -127,10 +141,11 @@ def causal_inference(
             }
         except ValueError as e:
             refute_results[method] = str(e)
+    output_buffer.close()
 
     # Create the summary string
     summary = f"Causal inference results for treatment variable '{treatment}' and outcome variable '{outcome}':\n"
-    summary += f"Estimated effect: {estimate.value:.2f}\n"
+    summary += f"Estimated effect: {estimate.value}\n"
     summary += "Refutation results:\n"
     for method, results in refute_results.items():
         if isinstance(results, str):
