9c24a1192c7bc3fdb2322c2b713816cec1a3ef84
eljas <eljas.roellin@ikmail.com>
2023-12-18 22:59:43 +0100

    forgot to commit ep.anndata._constants.py

---
+++ ehrapy/anndata/_constants.py
@@ -0,0 +1,8 @@
+# Typing Column
+# -----------------------
+# The column name and used values in adata.var for column types.
+
+EHRAPY_TYPE_KEY = "ehrapy_column_type"
+NUMERIC_TAG = "numeric"
+NON_NUMERIC_TAG = "non_numeric"
+NON_NUMERIC_ENCODED_TAG = "non_numeric_encoded"
