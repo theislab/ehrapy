47e5c07576bd78b051a0b18586b9eb97b507f04d
Lilly May <93096564+Lilly-May@users.noreply.github.com>
2024-04-07 18:16:33 +0200

    Calculation of feature importances in a supervised setting (#677)
    * Added feature importances function and tests
    
    * Added feature importances plotting function
    
    * Add function in init
    
    * Added evaluation on test set
    
    * Raise error for non-numeric features
    
    * Added output as percentage option
    
    * Harmonize plotting function
    
    * PR Reviews
    
    * Return Figure axes when show=False
    
    * Refactor API
    
    * Added docs examples
    
    * Auto prediction_type inference
    
    * Simplified error message
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    ---------
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>

---
+++ tests/tools/supervised/test_feature_importances.py
@@ -0,0 +1,56 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+import pytest
+from anndata import AnnData
+
+from ehrapy.tools import rank_features_supervised
+
+
+def test_continuous_prediction():
+    target = np.random.rand(1000)
+    X = np.stack((target, target * 2, [1] * 1000)).T
+    adata = AnnData(X)
+    adata.var_names = ["target", "feature1", "feature2"]
+
+    for model in ["regression", "svm", "rf"]:
+        rank_features_supervised(adata, "target", "continuous", model, "all")
+        assert "feature_importances" in adata.var
+        assert adata.var["feature_importances"]["feature1"] > 0
+        assert adata.var["feature_importances"]["feature2"] == 0
+        assert pd.isna(adata.var["feature_importances"]["target"])
+
+
+def test_categorical_prediction():
+    target = np.random.randint(2, size=1000)
+    X = np.stack((target, target, [1] * 1000)).T
+
+    adata = AnnData(X)
+    adata.var_names = ["target", "feature1", "feature2"]
+
+    for model in ["regression", "svm", "rf"]:
+        rank_features_supervised(adata, "target", "categorical", model, "all")
+        assert "feature_importances" in adata.var
+        assert adata.var["feature_importances"]["feature1"] > 0
+        assert adata.var["feature_importances"]["feature2"] == 0
+        assert pd.isna(adata.var["feature_importances"]["target"])
+
+
+def test_multiclass_prediction():
+    target = np.random.randint(4, size=1000)
+    X = np.stack((target, target, [1] * 1000)).T
+
+    adata = AnnData(X)
+    adata.var_names = ["target", "feature1", "feature2"]
+
+    rank_features_supervised(adata, "target", "categorical", "rf", "all")
+    assert "feature_importances" in adata.var
+    assert adata.var["feature_importances"]["feature1"] > 0
+    assert adata.var["feature_importances"]["feature2"] == 0
+    assert pd.isna(adata.var["feature_importances"]["target"])
+
+    for invalid_model in ["regression", "svm"]:
+        with pytest.raises(ValueError) as excinfo:
+            rank_features_supervised(adata, "target", "categorical", invalid_model, "all")
+        assert str(excinfo.value).startswith("Feature target has more than two categories.")
