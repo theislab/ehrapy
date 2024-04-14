e8a6e57614d21040b77d637343a3c4d0815d963b
eroell <eljas.roellin@ikmail.com>
2024-04-14 11:23:15 +0200

    fix links?

--- ehrapy/preprocessing/_balanced_sampling.py
+++ ehrapy/preprocessing/_balanced_sampling.py
@@ -23,8 +23,8 @@ def balanced_sample(
     Balancing groups in the dataset based on group members in `.obs[key]` using the [imbalanced-learn](https://imbalanced-learn.org/stable/index.html) package.
     Currently supports `RandomUnderSampler` and `RandomOverSampler`.
 
-    Note that [RandomOverSampler](https://imbalanced-learn.org/stable/references/generated/imblearn.over_sampling.RandomOverSampler.html) only replicates observations of the minority groups, which distorts several downstream analyses, very prominently neighborhood calculations and downstream analyses depending on that.
-    The [RandomUnderSampler](https://imbalanced-learn.org/stable/references/generated/imblearn.under_sampling.RandomUnderSampler.html) by  default undersamples the majority group without replacement, not causing this issues of replicated observations.
+    Note that `RandomOverSampler <https://imbalanced-learn.org/stable/references/generated/imblearn.over_sampling.RandomOverSampler.html>`_ only replicates observations of the minority groups, which distorts several downstream analyses, very prominently neighborhood calculations and downstream analyses depending on that.
+    The `RandomUnderSampler <https://imbalanced-learn.org/stable/references/generated/imblearn.under_sampling.RandomUnderSampler.html>`_ by  default undersamples the majority group without replacement, not causing this issues of replicated observations.
 
     Args:
         adata: The annotated data matrix of shape `n_obs` × `n_vars`.
