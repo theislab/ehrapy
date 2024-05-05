53c873279dfc043b40dfe4e77fdbefb6c5b27846
zethson <lukas.heumos@posteo.net>
2024-05-05 21:43:28 +0200

    Use __all__
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- ehrapy/data/__init__.py
+++ ehrapy/data/__init__.py
@@ -21,3 +21,27 @@ from ehrapy.data._datasets import (
     synthea_1k_sample,
     thyroid,
 )
+
+__all__ = [
+    "breast_cancer_coimbra",
+    "breast_tissue",
+    "cervical_cancer_risk_factors",
+    "chronic_kidney_disease",
+    "dermatology",
+    "diabetes_130_fairlearn",
+    "diabetes_130_raw",
+    "echocardiogram",
+    "heart_disease",
+    "heart_failure",
+    "hepatitis",
+    "mimic_2",
+    "mimic_2_preprocessed",
+    "mimic_3_demo",
+    "parkinson_dataset_with_replicated_acoustic_features",
+    "parkinsons",
+    "parkinsons_disease_classification",
+    "parkinsons_telemonitoring",
+    "statlog_heart",
+    "synthea_1k_sample",
+    "thyroid",
+]
--- ehrapy/preprocessing/__init__.py
+++ ehrapy/preprocessing/__init__.py
@@ -28,3 +28,35 @@ from ehrapy.preprocessing._outliers import clip_quantile, winsorize
 from ehrapy.preprocessing._quality_control import mcar_test, qc_lab_measurements, qc_metrics
 from ehrapy.preprocessing._scanpy_pp_api import *  # noqa: F403
 from ehrapy.preprocessing._summarize_measurements import summarize_measurements
+
+__all__ = [
+    "balanced_sample",
+    "detect_bias",
+    "encode",
+    "undo_encoding",
+    "highly_variable_features",
+    "explicit_impute",
+    "iterative_svd_impute",
+    "knn_impute",
+    "matrix_factorization_impute",
+    "mice_forest_impute",
+    "miss_forest_impute",
+    "nuclear_norm_minimization_impute",
+    "simple_impute",
+    "soft_impute",
+    "log_norm",
+    "maxabs_norm",
+    "minmax_norm",
+    "offset_negative_values",
+    "power_norm",
+    "quantile_norm",
+    "robust_scale_norm",
+    "scale_norm",
+    "sqrt_norm",
+    "clip_quantile",
+    "winsorize",
+    "mcar_test",
+    "qc_lab_measurements",
+    "qc_metrics",
+    "summarize_measurements",
+]
