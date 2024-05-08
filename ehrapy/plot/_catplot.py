4c92036f11a77078bd357c870bf67ac55057a84f
eroell <eljas.roellin@ikmail.com>
2024-05-08 19:33:44 +0200

    updates catplot

--- ehrapy/plot/_catplot.py
+++ ehrapy/plot/_catplot.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pandas as pd
 import seaborn as sns
 
 if TYPE_CHECKING:
@@ -9,13 +10,20 @@ if TYPE_CHECKING:
     from seaborn.axisgrid import FacetGrid
 
 
-def catplot(adata: AnnData, **kwargs) -> FacetGrid:
+def catplot(adata: AnnData, x: str = None, y: str = None, hue: str = None, kind: str = "strip", **kwargs) -> FacetGrid:
     """Plot categorical data.
 
-    Wrapper around seaborn.catplot.
+    Wrapper around seaborn.catplot. Typically used to show the behaviour of one numerical with respect to one
+    or several categorical variables.
+
+    Considers adata.obs only.
 
     Args:
         adata: AnnData object.
+        x: Variable to plot on the x-axis.
+        y: Variable to plot on the y-axis.
+        hue: Variable to plot as different colors.
+        kind: Kind of plot to make. Options are: "point", "bar", "strip", "swarm", "box", "violin", "boxen", or "count".
         **kwargs: Keyword arguments for seaborn.catplot.
 
     Returns:
@@ -38,4 +46,4 @@ def catplot(adata: AnnData, **kwargs) -> FacetGrid:
         .. image:: /_static/docstring_previews/catplot.png
     """
 
-    return sns.catplot(data=adata.obs, **kwargs)
+    return sns.catplot(data=adata.obs, x=x, y=y, hue=hue, kind=kind, **kwargs)
