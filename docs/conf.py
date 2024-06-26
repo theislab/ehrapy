975a0380986c3730f7e0bcbf6c3a30a798ec6628
Eljas <eljas.roellin@gmail.com>
2024-06-26 12:02:37 +0200

    try to add toggle functionality

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ extensions = [
     "sphinx_gallery.load_style",
     "sphinx_remove_toctrees",
     "sphinx_design",
+    "sphinx-togglebutton",
 ]
 
 # remove_from_toctrees = ["tutorials/notebooks/*", "api/reference/*"]
