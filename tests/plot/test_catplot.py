74bab7c49becc408c181602580434e6079441f2e
Carl Buchholz <32228189+aGuyLearning@users.noreply.github.com>
2024-12-06 15:58:18 +0100

    coxph_forestplot

--- tests/plot/test_catplot.py
+++ tests/plot/test_catplot.py
@@ -1,16 +1,27 @@
 from pathlib import Path
 
-from ehrapy.plot import catplot
+import ehrapy as ep
 
 CURRENT_DIR = Path(__file__).parent
 _TEST_IMAGE_PATH = f"{CURRENT_DIR}/_images"
 
 
 def test_catplot_vanilla(adata_mini, check_same_image):
-    fig = catplot(adata_mini, jitter=False)
+    fig = ep.pl.catplot(adata_mini, jitter=False)
 
     check_same_image(
         fig=fig,
         base_path=f"{_TEST_IMAGE_PATH}/catplot_vanilla",
+        tol=2e-1,
+    )
+
+def test_coxph_forestplot(mimic_2, check_same_image):
+    adata_subset = mimic_2[:, ["mort_day_censored", "censor_flg", "gender_num", "afib_flg", "day_icu_intime_num"]]
+    coxph = ep.tl.cox_ph(adata_subset, duration_col="mort_day_censored", event_col="censor_flg")
+    fig, ax = ep.pl.coxph_forestplot(coxph, fig_size=(12,3), t_adjuster=0.15, marker="o", size=2, text_size=14)
+
+    check_same_image(
+        fig=fig,
+        base_path=f"{_TEST_IMAGE_PATH}/coxph_forestplot",
         tol=2e-1,
     )
