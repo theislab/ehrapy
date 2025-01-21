ae9eed72b8bec37be90c6fb52d3f10ee18b56849
Carl Buchholz <32228189+aGuyLearning@users.noreply.github.com>
2025-01-21 15:31:23 +0100

    Enhancement: KM plot data depth and functionality (#853)
    * using current commit of tutorials notebook
    
    * Enhance Kaplan-Meier plot with grid layout and survival probability table
    
    * added toggle for table
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix return values in kaplan_meier and update documentation for cox_ph_forestplot
    
    * Fix typo in display_table parameter and add tests for Kaplan-Meier plots
    
    * Rename display_table parameter to display_survival_statistics in kaplan_meier function and update related tests
    
    * Fix documentation typo in cox_ph_forestplot function
    
    * Update grid specification in kaplan_meier function and rerendered test images
    
    * Improve docstring
    
    ---------
    
    Co-authored-by: Eljas Roellin <65244425+eroell@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>

--- tests/plot/test_survival_analysis.py
+++ tests/plot/test_survival_analysis.py
@@ -1,9 +1,53 @@
 from pathlib import Path
 
+import numpy as np
+
 import ehrapy as ep
 
 CURRENT_DIR = Path(__file__).parent
 _TEST_IMAGE_PATH = f"{CURRENT_DIR}/_images"
+
+
+def test_kaplan_meier(mimic_2, check_same_image):
+    mimic_2[:, ["censor_flg"]].X = np.where(mimic_2[:, ["censor_flg"]].X == 0, 1, 0)
+    groups = mimic_2[:, ["service_unit"]].X
+    adata_ficu = mimic_2[groups == "FICU"]
+    adata_micu = mimic_2[groups == "MICU"]
+    kmf_1 = ep.tl.kaplan_meier(adata_ficu, duration_col="mort_day_censored", event_col="censor_flg", label="FICU")
+    kmf_2 = ep.tl.kaplan_meier(adata_micu, duration_col="mort_day_censored", event_col="censor_flg", label="MICU")
+    fig, ax = ep.pl.kaplan_meier(
+        [kmf_1, kmf_2],
+        ci_show=[False, False, False],
+        color=["k", "r"],
+        xlim=[0, 750],
+        ylim=[0, 1],
+        xlabel="Days",
+        ylabel="Proportion Survived",
+    )
+
+    check_same_image(
+        fig=fig,
+        base_path=f"{_TEST_IMAGE_PATH}/kaplan_meier",
+        tol=2e-1,
+    )
+
+    fig, ax = ep.pl.kaplan_meier(
+        [kmf_1, kmf_2],
+        ci_show=[False, False, False],
+        color=["k", "r"],
+        xlim=[0, 750],
+        ylim=[0, 1],
+        xlabel="Days",
+        ylabel="Proportion Survived",
+        grid=True,
+        display_survival_statistics=True,
+    )
+
+    check_same_image(
+        fig=fig,
+        base_path=f"{_TEST_IMAGE_PATH}/kaplan_meier_table",
+        tol=2e-1,
+    )
 
 
 def test_coxph_forestplot(mimic_2, check_same_image):
