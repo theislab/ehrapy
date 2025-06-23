ec1fd737811628162c8b78a66f001692178a59e3
oykusuoglu <sueoglu@cip.ifi.lmu.de>
2025-06-23 09:09:32 +0200

    tests for plots

---
+++ tests/plot/test_missingno_pl_api.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import matplotlib
+matplotlib.use("Agg")
+import ehrapy as ep
+
+CURRENT_DIR = Path(__file__).parent
+_TEST_IMAGE_PATH = f"{CURRENT_DIR}/_images"
+
+def test_barplot(mimic_2, check_same_image):
+    plot = ep.pl.missing_values_barplot(mimic_2, filter="bottom", max_cols=15, max_percentage=0.999)
+    fig = plot.figure
+
+    check_same_image(
+        fig=fig,
+        base_path=f"{_TEST_IMAGE_PATH}/missing_values_barplot",
+        tol=2e-1,
+    )
+
+def test_matrix(mimic_2, check_same_image):
+    plot = ep.pl.missing_values_matrix(mimic_2, filter="bottom", max_cols=15, max_percentage=0.999)
+    fig = plot.figure
+
+    check_same_image(
+        fig=fig,
+        base_path=f"{_TEST_IMAGE_PATH}/missing_values_matrix",
+        tol=2e-1,
+    )
+
+def test_heatmap(mimic_2, check_same_image):
+    plot = ep.pl.missing_values_heatmap(mimic_2, filter="bottom", max_cols=15, max_percentage=0.999)
+    fig = plot.figure
+
+    check_same_image(
+        fig=fig,
+        base_path=f"{_TEST_IMAGE_PATH}/missing_values_heatmap",
+        tol=2e-1,
+    )
+
+def test_dendogram(mimic_2, check_same_image):
+    plot = ep.pl.missing_values_dendrogram(mimic_2, filter="bottom", max_cols=15, max_percentage=0.999)
+    fig = plot.figure
+
+    check_same_image(
+        fig=fig,
+        base_path=f"{_TEST_IMAGE_PATH}/missing_values_dendogram",
+        tol=2e-1,
+    )
