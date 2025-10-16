f5ad8a6735749f922d19618981c2b2bc10f0deff
Öykü Süoglu <oeykue.sueoglu@helmholtz-munich.de>
2025-10-16 12:05:50 +0000

    clean_up_plots fixture for pca_overview too as it produces some random plots

--- tests/plot/test_scanpy_pl_api.py
+++ tests/plot/test_scanpy_pl_api.py
@@ -480,7 +480,8 @@ def test_pca_variance_ration(mimic_2_sample_serv_unit_day_icu, check_same_image)
     plt.close("all")
 
 
-def test_pca_overview(mimic_2_sample_serv_unit_day_icu, check_same_image):
+def test_pca_overview(mimic_2_sample_serv_unit_day_icu, check_same_image, clean_up_plots):
+    plt.close("all")
     adata = mimic_2_sample_serv_unit_day_icu.copy()
     adata = adata[~np.isnan(adata.X).any(axis=1)].copy()
     adata = adata[:200, :].copy()
