8c8805beed35936f7b8762cb151fd7aadf11eca0
Öykü Süoglu <oeykue.sueoglu@helmholtz-munich.de>
2025-10-16 11:46:36 +0000

    adding pca_overview test

--- tests/plot/test_scanpy_pl_api.py
+++ tests/plot/test_scanpy_pl_api.py
@@ -480,7 +480,6 @@ def test_pca_variance_ration(mimic_2_sample_serv_unit_day_icu, check_same_image)
     plt.close("all")
 
 
-"""
 def test_pca_overview(mimic_2_sample_serv_unit_day_icu, check_same_image):
     adata = mimic_2_sample_serv_unit_day_icu.copy()
     adata = adata[~np.isnan(adata.X).any(axis=1)].copy()
@@ -506,8 +505,6 @@ def test_pca_overview(mimic_2_sample_serv_unit_day_icu, check_same_image):
             tol=2e-1,
         )
     plt.close("all")
-
-"""
 
 
 def test_umap_functionality(mimic_2_sample_serv_unit_day_icu):
