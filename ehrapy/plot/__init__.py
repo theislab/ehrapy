f67248737429c8ee2ca9ee626f641d4b1bb96306
Öykü Süoglu <oeykue.sueoglu@helmholtz-munich.de>
2025-12-10 10:20:39 +0100

    sankey added to init.py

--- ehrapy/plot/__init__.py
+++ ehrapy/plot/__init__.py
@@ -12,5 +12,6 @@ from ehrapy.plot._scanpy_pl_api import *  # noqa: F403
 from ehrapy.plot._survival_analysis import cox_ph_forestplot, kaplan_meier, ols
 from ehrapy.plot.causal_inference._dowhy import causal_effect
 from ehrapy.plot.feature_ranking._feature_importances import rank_features_supervised
+from ehrapy.plot._sankey import plot_sankey, plot_sankey_time
 
 hv.extension("bokeh")
