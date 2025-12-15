195e827a991bd10bf3f920d1758cf3d041ceffaf
Lukas Heumos <lukas.heumos@posteo.net>
2025-12-15 17:40:26 +0100

    explicit imports
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- ehrapy/plot/__init__.py
+++ ehrapy/plot/__init__.py
@@ -8,7 +8,40 @@ from ehrapy.plot._missingno import (
     missing_values_heatmap,
     missing_values_matrix,
 )
-from ehrapy.plot._scanpy_pl_api import *  # noqa: F403
+from ehrapy.plot._scanpy_pl_api import (
+    clustermap,
+    dendrogram,
+    diffmap,
+    dotplot,
+    dpt_groups_pseudotime,
+    dpt_timeseries,
+    draw_graph,
+    embedding,
+    embedding_density,
+    heatmap,
+    matrixplot,
+    paga,
+    paga_compare,
+    paga_path,
+    pca,
+    pca_loadings,
+    pca_overview,
+    pca_variance_ratio,
+    rank_features_groups,
+    rank_features_groups_dotplot,
+    rank_features_groups_heatmap,
+    rank_features_groups_matrixplot,
+    rank_features_groups_stacked_violin,
+    rank_features_groups_tracksplot,
+    rank_features_groups_violin,
+    ranking,
+    scatter,
+    stacked_violin,
+    tracksplot,
+    tsne,
+    umap,
+    violin,
+)
 from ehrapy.plot._survival_analysis import cox_ph_forestplot, kaplan_meier, ols
 from ehrapy.plot.causal_inference._dowhy import causal_effect
 from ehrapy.plot.feature_ranking._feature_importances import rank_features_supervised
