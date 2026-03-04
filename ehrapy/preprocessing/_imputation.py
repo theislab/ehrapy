9c84ca080704cee926c22df36271ee72c7801e07
Öykü Süoglu <oykusuogluu@gmail.com>
2026-03-04 10:23:17 +0100

    removed spinner

--- ehrapy/preprocessing/_imputation.py
+++ ehrapy/preprocessing/_imputation.py
@@ -30,8 +30,6 @@ if TYPE_CHECKING:
 
 
 @use_ehrdata(deprecated_after="1.0.0")
-@function_2D_only()
-@spinner("Performing explicit impute")
 def explicit_impute(
     edata: EHRData | AnnData,
     replacement: (str | int) | (dict[str, str | int]),
