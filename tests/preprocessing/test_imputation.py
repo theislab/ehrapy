6eb20f34d876fa0b84a4bd886694d48f2f0c41a6
oykusuoglu <sueoglu@cip.ifi.lmu.de>
2026-03-11 23:47:08 +0300

    test_miceforest_impute_numerical_data was failing due to copy=True

--- tests/preprocessing/test_imputation.py
+++ tests/preprocessing/test_imputation.py
@@ -397,7 +397,7 @@ def test_miceforest_impute_non_numerical_data(impute_titanic_edata):
 @pytest.mark.filterwarnings("ignore:invalid value encountered in divide:RuntimeWarning")
 def test_miceforest_impute_numerical_data(impute_iris_edata):
     edata_not_imputed = impute_iris_edata.copy()
-    mice_forest_impute(impute_iris_edata, copy=True)
+    mice_forest_impute(impute_iris_edata)
 
     _base_check_imputation(edata_not_imputed, impute_iris_edata)
 
